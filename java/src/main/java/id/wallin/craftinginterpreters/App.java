e6afbddbcaac85e5d88310b6d1a935881c8d7509
Jussi Wallin <jussi@beanbakers.fi>
2023-03-22 17:35:39 +0200

    Add hello world projects

---
+++ java/src/main/java/id/wallin/craftinginterpreters/App.java
@@ -0,0 +1,17 @@
+package id.wallin.craftinginterpreters;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    private static Logger LOG = LoggerFactory.getLogger(App.class);
+    public static void main( String[] args )
+    {
+        LOG.info( "Hello World!" );
+    }
+}
---
+++ java/src/test/java/id/wallin/craftinginterpreters/AppTest.java
@@ -0,0 +1,18 @@
+package id.wallin.craftinginterpreters;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest
+{
+
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
